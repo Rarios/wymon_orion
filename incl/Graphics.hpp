4b9659c7f6911bff9cca5225d9fe5f795e27dc50
Seb Muek <seb.muek.prog@gmail.com>
2015-01-03 00:45:55 +0100

    Included SFML source files into project. Changed SFML source include directoriy names. Added SFML examples and extlib folder. Created new CMakeLists.txt structure.

---
+++ incl/Graphics.hpp
@@ -0,0 +1,62 @@
+////////////////////////////////////////////////////////////
+//
+// SFML - Simple and Fast Multimedia Library
+// Copyright (C) 2007-2014 Laurent Gomila (laurent.gom@gmail.com)
+//
+// This software is provided 'as-is', without any express or implied warranty.
+// In no event will the authors be held liable for any damages arising from the use of this software.
+//
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it freely,
+// subject to the following restrictions:
+//
+// 1. The origin of this software must not be misrepresented;
+//    you must not claim that you wrote the original software.
+//    If you use this software in a product, an acknowledgment
+//    in the product documentation would be appreciated but is not required.
+//
+// 2. Altered source versions must be plainly marked as such,
+//    and must not be misrepresented as being the original software.
+//
+// 3. This notice may not be removed or altered from any source distribution.
+//
+////////////////////////////////////////////////////////////
+
+#ifndef SFML_GRAPHICS_HPP
+#define SFML_GRAPHICS_HPP
+
+////////////////////////////////////////////////////////////
+// Headers
+////////////////////////////////////////////////////////////
+
+#include <Window.hpp>
+#include <graphics/BlendMode.hpp>
+#include <graphics/Color.hpp>
+#include <graphics/Font.hpp>
+#include <graphics/Glyph.hpp>
+#include <graphics/Image.hpp>
+#include <graphics/RenderStates.hpp>
+#include <graphics/RenderTexture.hpp>
+#include <graphics/RenderWindow.hpp>
+#include <graphics/Shader.hpp>
+#include <graphics/Shape.hpp>
+#include <graphics/CircleShape.hpp>
+#include <graphics/RectangleShape.hpp>
+#include <graphics/ConvexShape.hpp>
+#include <graphics/Sprite.hpp>
+#include <graphics/Text.hpp>
+#include <graphics/Texture.hpp>
+#include <graphics/Transform.hpp>
+#include <graphics/Vertex.hpp>
+#include <graphics/VertexArray.hpp>
+#include <graphics/View.hpp>
+
+
+#endif // SFML_GRAPHICS_HPP
+
+////////////////////////////////////////////////////////////
+/// \defgroup graphics Graphics module
+///
+/// 2D graphics module: sprites, text, shapes, ...
+///
+////////////////////////////////////////////////////////////

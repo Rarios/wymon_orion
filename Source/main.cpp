37a4ff72f24a813b6b33dadc31a477f1468cc701
Seb Muek <seb.muek.prog@gmail.com>
2015-02-03 19:33:32 +0100

    Renamed Include folders 'graphics' and 'utils' to written out, uppercase version. Modified source directory for window icon in 'main.cpp'.

--- Source/main.cpp
+++ Source/main.cpp
@@ -13,7 +13,7 @@ signed int main ( void ) {
 	Orion Win(sf::VideoMode(600, 450), "WymonOrion - angad 0.0.0");
 
 	// Create Icon
-	if (!(Win.win_icon("res/wymonIcon.png"))) {
+	if (!(Win.win_icon("Resources/wymonIcon.png"))) {
 	
 		std::cerr << "Could not load window icon";
 		std::cin.get();

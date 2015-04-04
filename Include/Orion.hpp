1bf9a03e9953b9f3d98e92e2724c2aded0f88e1e
yozoon <julius@piso.at>
2015-04-04 16:08:24 +0200

    Resetted files after "TextureManager" testing.

--- Include/Orion.hpp
+++ Include/Orion.hpp
@@ -1,95 +1,95 @@
-// // Orion
-// // Orion.hpp
+// Orion
+// Orion.hpp
 
-// //#define NDEBUG
-// #ifndef _ORION_
-// #define _ORION_
+//#define NDEBUG
+#ifndef _ORION_
+#define _ORION_
 
-// #include <iostream>
-// #ifndef SFML_GRAPHICS_HPP
-// #include <SFML/Graphics.hpp>
-// #endif
-// #ifndef GRAPHICS_SPRITE
-// #include "Sprite.hpp"
-// #endif
-// #ifndef GRAPHICS_TEXT
-// #include "Text.hpp"
-// #endif
-// #ifndef _TEXTFIELD_
-// #include "Textfield.hpp"
-// #endif
-// #ifndef GRAPHICS_ANIMATION
-// #include "Animation.hpp"
-// #endif
-// #ifndef SYSTEM_TIMESTRING
-// #include "TimeString.hpp"
-// #endif
+#include <iostream>
+#ifndef SFML_GRAPHICS_HPP
+#include <SFML/Graphics.hpp>
+#endif
+#ifndef GRAPHICS_SPRITE
+#include "Sprite.hpp"
+#endif
+#ifndef GRAPHICS_TEXT
+#include "Text.hpp"
+#endif
+#ifndef _TEXTFIELD_
+#include "Textfield.hpp"
+#endif
+#ifndef GRAPHICS_ANIMATION
+#include "Animation.hpp"
+#endif
+#ifndef SYSTEM_TIMESTRING
+#include "TimeString.hpp"
+#endif
 
-// // Orion //
-// // Orion is the name of the m_wymon "Terminal". It's a program
-// // desinged to support the user in his daily work. All of the
-// // functionalities are combined into this one main class
+// Orion //
+// Orion is the name of the m_wymon "Terminal". It's a program
+// desinged to support the user in his daily work. All of the
+// functionalities are combined into this one main class
 
-// //! Orion class.
-// /*!
-// * This class holds the main entry point for the program, as well as all the
-// * data needed by the program, such as background, animation, etc.
-// */
-// class Orion {
+//! Orion class.
+/*!
+* This class holds the main entry point for the program, as well as all the
+* data needed by the program, such as background, animation, etc.
+*/
+class Orion {
 
-// private :
+private :
 
-// 	//! Window used as a render target.
-// 	sf::RenderWindow m_win;
-// 	//! Window icon.
-// 	sf::Image m_win_icon;
+	//! Window used as a render target.
+	sf::RenderWindow m_win;
+	//! Window icon.
+	sf::Image m_win_icon;
 
-// 	//! Window time and date string handling.
-// 	wo::sys::TimeString mTimeString;
+	//! Window time and date string handling.
+	wo::sys::TimeString mTimeString;
 
-// 	//! Window background.
-// 	wo::gfx::Sprite m_background;
+	//! Window background.
+	wo::gfx::Sprite m_background;
 
-// 	//! Wymon animation.
-// 	wo::gfx::Animation m_wymon;
-// 	//! Clock for the Animation
-// 	sf::Clock m_clock;
-// 	//! Elapsed time since last frame.
-// 	/*!
-// 	* This variable stores the time that has been passed since the last time
-// 	* the animation rendered a frame. If this hits a limit, the animation
-// 	* should render again.
-// 	*/
-// 	sf::Time m_elap_time;
+	//! Wymon animation.
+	wo::gfx::Animation m_wymon;
+	//! Clock for the Animation
+	sf::Clock m_clock;
+	//! Elapsed time since last frame.
+	/*!
+	* This variable stores the time that has been passed since the last time
+	* the animation rendered a frame. If this hits a limit, the animation
+	* should render again.
+	*/
+	sf::Time m_elap_time;
 
-// 	//! Font used for all text inside the window.
-// 	sf::Font m_font;
-// 	//! Path to the font used in this class.
-// 	std::string m_fontname;
+	//! Font used for all text inside the window.
+	sf::Font m_font;
+	//! Path to the font used in this class.
+	std::string m_fontname;
 
-// 	//! Text displaying the current time.
-// 	wo::gfx::Text m_time_text;
-// 	//! Text displaying the current date.
-// 	wo::gfx::Text m_date_text;
+	//! Text displaying the current time.
+	wo::gfx::Text m_time_text;
+	//! Text displaying the current date.
+	wo::gfx::Text m_date_text;
 
-// 	//! Input field for the user.
-// 	Textfield m_textfield ;
+	//! Input field for the user.
+	Textfield m_textfield ;
 
-// 	void obj_pos();
-// 	void render();
+	void obj_pos();
+	void render();
 
-// public :
+public :
 
-// 	Orion(sf::VideoMode mode , const sf::String& title , sf::Uint32 style = sf::Style::Default ,
-// 			const sf::ContextSettings& settings = sf::ContextSettings ());
-// 	~Orion();
+	Orion(sf::VideoMode mode , const sf::String& title , sf::Uint32 style = sf::Style::Default ,
+			const sf::ContextSettings& settings = sf::ContextSettings ());
+	~Orion();
 
-// 	bool win_icon(const std::string& filename);
+	bool win_icon(const std::string& filename);
 
-// 	void proc_events();
-// 	void draw_obj() ;
-// 	void run();
+	void proc_events();
+	void draw_obj() ;
+	void run();
 
-// } ;
+} ;
 
-// #endif
+#endif
